d575bfa0f04ebbdac13408fef60f3d29c017fc8d
Calvin Lui <clui951@berkeley.edu>
2016-05-16 22:47:46 -0700

    set twilio credentials ; import dependencies

--- src/app.py
+++ src/app.py
@@ -1,6 +1,15 @@
+import os
 from flask import Flask
+import twilio.twiml
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
+
+
+# Find these values at https://twilio.com/user/account
+account_sid = os.environ.get('TWILIO_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+client = TwilioRestClient(account_sid, auth_token)
 
 
 ################### Routes #####################
